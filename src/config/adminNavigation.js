b4d14912d92c53440a798f7a0daedbcb60daf100
Samah BaniOdeh <samahbaniodeh1@gmail.com>
2025-07-11 10:05:55 +0000

    chore: delete unnecessary file

--- src/config/adminNavigation.js
+++
@@ -1,5 +0,0 @@
-export const adminNavigation = [
-  { label: 'Manage Cities', path: '/admin/cities' },
-  { label: 'Manage Hotels', path: '/admin/hotels' },
-  { label: 'Manage Rooms', path: '/admin/rooms' },
-];
